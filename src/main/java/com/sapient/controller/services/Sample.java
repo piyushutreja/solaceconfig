a71e85d2afe713a3ce2c901af12208046c2d2eb1
SAPIENT\putrej <piyushutreja@gmail.com>
2017-05-24 10:52:55 +0530

    Added SSL config

--- src/main/java/com/sapient/controller/services/Sample.java
+++ src/main/java/com/sapient/controller/services/Sample.java
@@ -1,4 +1,6 @@
 package com.sapient.controller.services;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.stereotype.Component;
 import com.sapient.controller.aspects.Loggable;
 import com.sapient.controller.aspects.Loggable.Level;
@@ -10,7 +12,12 @@ public class Sample {
 	
 	public void doStuff()
 	{
-		
+		try {
+			TimeUnit.MILLISECONDS.sleep(2);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("do Stuff console");
 	
 		
